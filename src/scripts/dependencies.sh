627bd8491a3ab60dfce789afb7769b69da945b3d
Sigrid Andersson <sigrid.andersson@entur.org>
2022-08-26 10:37:13 +0200

    Move scripts to files

---
+++ src/scripts/dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+go install github.com/google/go-jsonnet/cmd/jsonnet@latest
+go install github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb@latest
---
+++ src/scripts/upload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "$IS_UPDATED"
+
+if [[ "1" == "$IS_UPDATED" ]]; then
+  echo "Updating remote dashboard"
+
+  GIT_COMMIT_DESC="$(git log --format=oneline -n 1 "$CIRCLE_SHA1")"
+  BODY="{
+        \"dashboard\": $(jq . "$DASHBOARD_NAME".json),
+        \"folderUid\": \"$FOLDER_UID\",
+        \"overwrite\": true,
+        \"message\": \"$GIT_COMMIT_DESC\"
+  }"
+  curl "$GRAFANA_DASHBOARDS_HOST"/api/dashboards/db -d "$BODY" \
+        -H "Authorization: Bearer $GRAFANA_DASHBOARDS_KEY" \
+        -H "Content-type: application/json"
+  exit $?
+fi
