88cb2a155b9deff6584a6bb85616757f6d1a5dd8
Sigrid Andersson <sigrid.andersson@entur.org>
2022-09-06 15:42:48 +0200

    Use best practise for curl using file

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -6,13 +6,13 @@ if [[ "0" == "$IS_UPDATED" ]]; then
   echo "Updating remote dashboard"
 
   GIT_COMMIT_DESC="$(git log --format=oneline -n 1)"
-  BODY="{
+  echo "{
         \"dashboard\": $(jq . "$DASHBOARD_NAME".json),
         \"folderUid\": \"$FOLDER_UID\",
         \"overwrite\": true,
         \"message\": \"$GIT_COMMIT_DESC\"
-  }"
-  curl "$GRAFANA_DASHBOARDS_HOST"/api/dashboards/db -d "$BODY" \
+  }" > body.json
+  curl "$GRAFANA_DASHBOARDS_HOST"/api/dashboards/db -d @body.json \
         -H "Authorization: Bearer $GRAFANA_DASHBOARDS_KEY" \
         -H "Content-type: application/json"
   exit $?
